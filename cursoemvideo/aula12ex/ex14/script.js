efac4881f672a48e3cb1890fe9abfd135bf0f739
conceicaodomingos <conceicaojpdomingos@gmail.com>
2024-08-17 09:38:44 +0100

    atualizado ex14

--- cursoemvideo/aula12ex/ex14/script.js
+++ cursoemvideo/aula12ex/ex14/script.js
@@ -2,19 +2,24 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date ()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    //var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.\n`
 
-    if (hora >= 0 && hora < 12) {
-        msg.innerHTML += ` Bom dia!`
+    if (hora >= 0 && hora < 5) {
+        msg.innerHTML += `<strong>Boa Madrugada</strong>`
+        img.src = './imagens/foto-madrugada.jpg'
+        document.body.style.background = '#000000'
+    }
+    else if (hora <= 12) {
+        msg.innerHTML += `<strong>Bom dia!</strong>`
         img.src = './imagens/foto-manha.jpg'
         document.body.style.background = '#e2cd9f'
     } else if (hora <= 17) {
-        msg.innerHTML += ` Boa tarde!`
+        msg.innerHTML += `<strong>Boa tarde!</strong>`
         img.src = './imagens/foto-tarde.jpg' 
         document.body.style.background = '#b9846f'
     } else {
-        msg.innerHTML += ` Boa noite!`
+        msg.innerHTML += `<strong>Boa noite!</strong>`
         img.src = './imagens/foto-noite.jpg'
         document.body.style.background = '#515154'
     }
